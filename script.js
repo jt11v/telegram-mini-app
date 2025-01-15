68a5cac435dd412178b5c7eb69ab21b5095f38c6
Anton <jt.11@mail.ru>
2025-01-15 07:25:04 +0300

    change

--- script.js
+++ script.js
@@ -1,25 +1,96 @@
 
+// const tg = window.Telegram.WebApp;
+
+// tg.expand();
+
+// tg.MainButton.setText("Отправить");
+// tg.MainButton.show();
+
+// tg.MainButton.onClick(() => {
+//     const message = document.getElementById('messageInput').value;
+//     if (message) {
+//         tg.sendData(message); 
+//     } else {
+//         tg.showAlert("Введите сообщение!"); 
+//     }
+// });
+
+// document.getElementById('sendButton').addEventListener('click', () => {
+//     const message = document.getElementById('messageInput').value;
+//     if (message) {
+//         tg.sendData(message); 
+//     } else {
+//         tg.showAlert("Введите сообщение!"); 
+//     }
+// });
+
+
+
+
 const tg = window.Telegram.WebApp;
+tg.expand(); 
 
-tg.expand();
 
-tg.MainButton.setText("Отправить");
-tg.MainButton.show();
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
 
-tg.MainButton.onClick(() => {
-    const message = document.getElementById('messageInput').value;
-    if (message) {
-        tg.sendData(message); 
-    } else {
-        tg.showAlert("Введите сообщение!"); 
+
+let tasks = [];
+
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText === '') {
+        alert('Введите задачу!');
+        return;
+    }
+
+    
+    tasks.push(taskText);
+
+    
+    taskInput.value = '';
+
+    
+    renderTasks();
+}
+
+
+function deleteTask(index) {
+    tasks.splice(index, 1); 
+    renderTasks(); 
+}
+
+
+function renderTasks() {
+    
+    taskList.innerHTML = '';
+
+    
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.textContent = task;
+
+        
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Удалить';
+        deleteButton.addEventListener('click', () => deleteTask(index));
+
+        li.appendChild(deleteButton);
+        taskList.appendChild(li);
+    });
+}
+
+
+addButton.addEventListener('click', addTask);
+
+
+taskInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addTask();
     }
 });
 
-document.getElementById('sendButton').addEventListener('click', () => {
-    const message = document.getElementById('messageInput').value;
-    if (message) {
-        tg.sendData(message); 
-    } else {
-        tg.showAlert("Введите сообщение!"); 
-    }
-});
+
+tg.ready();
